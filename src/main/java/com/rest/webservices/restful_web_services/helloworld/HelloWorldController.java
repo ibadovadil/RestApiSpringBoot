802796c735b4e4d349d2bf3057476918a26feec5
ibadovadil <adilibadov654@gmail.com>
2025-07-20 18:42:41 +0400

    Add i18n

--- src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello")  //this return string
     public String hello() {
         return "Hello World";
@@ -21,5 +32,14 @@ public class HelloWorldController {
         return new HelloBean(
                 String.format("Hello %s", name)
         );
+    }
+
+//    send in Header Accept-Language :az or different languages or null
+    @GetMapping("/hello-i18n")
+    public String helloi18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+       return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+//        return "Hello World V2";
     }
 }
